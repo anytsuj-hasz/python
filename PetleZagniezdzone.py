6fb373b743b45d8efb448e85813cb760e5e700a9
anytsuj <justyna.h88@gmail.com>
2021-09-22 13:52:46 +0200

    change file, add dictionary

--- PetleZagniezdzone.py
+++ PetleZagniezdzone.py
@@ -1,3 +1,4 @@
+"""
 ListaGosci = [
     ('Anna', 28, 'kobieta', 123456),
     ('Jan', 32, 'mezczyzna', 564786),
@@ -13,3 +14,23 @@ for imie, wiek, plec, telefon in ListaGosci:
     print("Plec: ", plec)
     print("Telefon: ", telefon)
     print("\n")
+"""
+oceny = {
+    "Arkadiusz": (2, 5, 3, 4, 3),
+    "Anna": (3, 4, 2, 3)
+}
+
+for klucz in oceny:
+    print(klucz, "oceny: ", oceny[klucz])
+
+people = {
+        "abcd1234": {'name': 'Jan', 'age': 27, 'sex': 'male'},
+        "gfdt5678": {'name': 'Anna', 'age': 34, 'sex': 'female'},
+        "trwf0987": {'name': 'Pawel', 'age': 30, 'sex': 'male'},
+        "cbhg6537": {'name': 'Olga', 'age': 27, 'sex': 'female'}
+        }
+
+for id, dictionary in people.items():
+    print("ID: ", id)
+    for key in dictionary:
+        print(key, ": ", dictionary[key])
